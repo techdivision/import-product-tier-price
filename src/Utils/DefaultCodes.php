10991217d3b5c2093644438349b4ccbbaecd80a6
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2021-09-16 19:09:36 +0200

    Cleanup code and 3.8.x compatibility

--- src/Utils/DefaultCodes.php
+++ src/Utils/DefaultCodes.php
@@ -3,16 +3,10 @@
 /**
  * TechDivision\Import\Product\TierPrice\Utils\DefaultCodes
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * PHP version 5
+ * PHP version 7
  *
  * @author    Tim Wagner <t.wagner@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-product-tier-price
  * @link      https://www.techdivision.com
  */
@@ -23,7 +17,7 @@ namespace TechDivision\Import\Product\TierPrice\Utils;
  * Specific column keys for handling tier prices.
  *
  * @author    Tim Wagner <t.wagner@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-product-tier-price
  * @link      https://www.techdivision.com
  */
